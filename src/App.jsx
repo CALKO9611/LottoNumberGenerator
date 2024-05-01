9fbbbf2e524532271706af708c54ceb5b491f09b
CALKO9611 <kimd9669@naver.com>
2024-05-01 16:14:40 +0900

    CSS 초기화 및 수정

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,14 @@
-// import { useState } from "react";
 import "./App.css";
 import Header from "./components/header";
 import Content from "./components/content";
+import Footer from "./components/footer";
 
 function App() {
   return (
     <>
       <Header />
       <Content />
+      <Footer />
     </>
   );
 }
