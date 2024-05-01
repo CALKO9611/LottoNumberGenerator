b18ca2bc32835ca3ef7594acebdfdf4a2d31646d
CALKO9611 <kimd9669@naver.com>
2024-05-01 15:08:34 +0900

    header 초안 작성

---
+++ src/components/header.jsx
@@ -0,0 +1,31 @@
+import styled from "styled-components";
+
+const HeaderComponent = styled.header`
+  display: flex;
+  align-items: center;
+  /* justify-content: center; */
+  padding: 10px;
+  background-color: #f0f0f0;
+  border-bottom: 2px solid #ccc;
+
+  img {
+    width: 50px;
+    height: 50px;
+    margin-right: 10px;
+  }
+
+  h1 {
+    font-size: 24px;
+    font-weight: bold;
+    color: #333;
+  }
+`;
+
+export default function Header() {
+  return (
+    <HeaderComponent>
+      <img src="/public/LottoNumberGenerator icon.svg" alt="로번생 로고" />
+      <h1>로번생</h1>
+    </HeaderComponent>
+  );
+}
